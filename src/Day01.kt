a07e0d28259d1d94adf81f332fa1ddf8bbff537b
Niels van Hove <niels.vanhove@lightyear.one>
2022-12-01 23:29:36 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,49 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return getLargestStack(input)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return getStacks(input)
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+private fun getLargestStack(input: List<String>): Int {
+    var largest = 0
+    var currentStack = 0
+    for (line in input) {
+        if (line.isNotEmpty()) {
+            currentStack += line.toInt()
+        } else {
+            largest = maxOf(largest, currentStack)
+            currentStack = 0
+        }
+    }
+
+    largest = maxOf(largest, currentStack)
+    return largest
+}
+
+private fun getStacks(input: List<String>): List<Int> {
+    val list = mutableListOf<Int>()
+
+    var currentStack = 0
+    for (line in input) {
+        if (line.isNotEmpty()) {
+            currentStack += line.toInt()
+        } else {
+            list.add(currentStack)
+            currentStack = 0
+        }
+    }
+
+    list.add(currentStack)
+    return list
 }
